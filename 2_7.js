b6b97b0ffe1c4f1d72fef33e1b08061e09ee50da
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-11-02 14:49:45 -0800

    Creates Javascript stack class utility

---
+++ 2_7.js
@@ -0,0 +1,13 @@
+// Intersection
+
+// Assumptions:
+// - Singly linked list
+// -
+
+// Time Complexity:
+// Space Complexity:
+
+// Alternate Approaches:
+// -
+
+function intersection(head1, head2) {}
---
+++ Utilities/Stack.js
@@ -0,0 +1,19 @@
+class Stack {
+  constructor(data) {
+    this.data = []
+    this.top = null
+  }
+
+  push(value) {
+    this.data.push(value)
+    this.top = value
+  }
+
+  pop() {
+    const value = this.data.pop()
+    this.top = this.data[this.data.length - 1]
+    return value
+  }
+}
+
+exports.Stack = Stack
